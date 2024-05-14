54b262afa68a6133f549f062a064d00e6cdca41f
hui3.wang <hui3.wang@lenovonetapp.com>
2024-05-14 17:49:42 +0800

    search-installer

--- src/pages/main/search/1.tsx
+++ src/pages/main/search/1.tsx
@@ -10,7 +10,7 @@ import {Access, history } from "umi";
 import React, { useRef, useState, useEffect } from 'react';
 import styles from './index.less';
 import ProCard from "@ant-design/pro-card";
-import {appSetupTerraSearchClusterNodeGet} from "@/services/dsm/terraSearchDeploy";
+import {appSetupTerraSearchClusterNode, appSetupTerraSearchClusterNodeGet} from "@/services/dsm/terraSearchDeploy";
 
 const One = (props) => {
   const [form] = Form.useForm();
@@ -31,6 +31,8 @@ const One = (props) => {
   }, [])
 
   const onFinish = (values) => {
+    const res = await appSetupTerraSearchClusterNode(values);
+    console.log(res)
     history.push('2')
   }
 
@@ -72,18 +74,18 @@ const One = (props) => {
                         {form.getFieldValue('deploy_mode') === `standard` ? `标准部署的确可以支持部署在3到10个节点上，这种部署方式通常用于较大规模的检索集群，以提供更高的性能和可靠性` : `快速部署支持单节点部署，这种部署方式通常能够帮助客户在资源有限的情况下快速部署检索服务`}
                       </Form.Item>
                     
-                    <Form.Item label={'节点IP'} name={'节点IP'}>
+                    <Form.Item label={'节点IP'} name={'ip_address'}>
                       {form.getFieldValue('deploy_mode') === `standard` ? <Input.TextArea rows={4} placeholder={`3≤节点数量≤10`}/> : <Input placeholder={`请输入一个节点IP`}/>}
                     </Form.Item>
                   </>
                 )}
               </Form.Item>
                   
-              <Form.Item label={'用户名'} name={'用户名'}>
+              <Form.Item label={'用户名'} name={'username'}>
                 <Input/>
               </Form.Item>
 
-              <Form.Item label={'密码'} name={'密码'}>
+              <Form.Item label={'密码'} name={'password'}>
                 <Input/>
               </Form.Item>
 
